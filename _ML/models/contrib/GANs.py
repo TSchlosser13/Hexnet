1fae47d1aba22c61bc449979cbf986b77c099b52
Tobias Schlosser <tobias@tobias-schlosser.net>
2020-01-19 23:36:51 +0100

    [ML] updated GANs

--- _ML/models/contrib/GANs.py
+++ _ML/models/contrib/GANs.py
@@ -35,29 +35,29 @@ import tensorflow as tf
 
 from matplotlib.pyplot       import imsave
 from tensorflow.keras        import Input, Model, Sequential
-from tensorflow.keras.layers import Activation, BatchNormalization, Conv2D, Dense, Dropout, Embedding, Flatten, LeakyReLU, multiply, Reshape, UpSampling2D, ZeroPadding2D
+from tensorflow.keras.layers import Activation, BatchNormalization, Conv2D, Dense, Dropout, Embedding, Flatten, LeakyReLU, multiply, Reshape, UpSampling2D
 
 from misc.misc import Hexnet_print
 
 
 class ACGAN():
-    def __init__(self):
+    def __init__(self, input_shape, classes, optimizer='adam'):
         # Input shape
-        self.img_rows = 28
-        self.img_cols = 28
-        self.channels = 1
+        self.img_rows = input_shape[0]
+        self.img_cols = input_shape[1]
+        self.channels = input_shape[2]
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
-        self.num_classes = 10
+        self.num_classes = classes
         self.latent_dim = 100
 
-        optimizer = Adam(0.0002, 0.5)
+        if type(optimizer) is str:
+            optimizer = tf.optimizers.get(optimizer)
+
         losses = ['binary_crossentropy', 'sparse_categorical_crossentropy']
 
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss=losses,
-            optimizer=optimizer,
-            metrics=['accuracy'])
+        self.discriminator.compile(loss=losses, optimizer=optimizer, metrics=['accuracy'])
 
         # Build the generator
         self.generator = self.build_generator()
@@ -78,15 +78,14 @@ class ACGAN():
         # The combined model  (stacked generator and discriminator)
         # Trains the generator to fool the discriminator
         self.combined = Model([noise, label], [valid, target_label])
-        self.combined.compile(loss=losses,
-            optimizer=optimizer)
+        self.combined.compile(loss=losses, optimizer=optimizer)
+
 
     def build_generator(self):
-
         model = Sequential()
 
-        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
-        model.add(Reshape((7, 7, 128)))
+        model.add(Dense(128 * 8 * 8, activation="relu", input_dim=self.latent_dim))
+        model.add(Reshape((8, 8, 128)))
         model.add(BatchNormalization(momentum=0.8))
         model.add(UpSampling2D())
         model.add(Conv2D(128, kernel_size=3, padding="same"))
@@ -110,15 +109,14 @@ class ACGAN():
 
         return Model([noise, label], img)
 
-    def build_discriminator(self):
 
+    def build_discriminator(self):
         model = Sequential()
 
         model.add(Conv2D(16, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
         model.add(Conv2D(32, kernel_size=3, strides=2, padding="same"))
-        model.add(ZeroPadding2D(padding=((0,1),(0,1))))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dropout(0.25))
         model.add(BatchNormalization(momentum=0.8))
@@ -144,22 +142,19 @@ class ACGAN():
 
         return Model(img, [validity, label])
 
-    def train(self, epochs, batch_size=128, sample_interval=50):
 
+    def train(self, train_data, train_labels, batch_size=128, epochs=10000, tests_dir=None, run_title=None, images_to_sample_per_class=100, sample_rate=100):
         # Load the dataset
-        (X_train, y_train), (_, _) = mnist.load_data()
+        X_train, y_train = train_data, train_labels
 
         # Configure inputs
-        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
-        X_train = np.expand_dims(X_train, axis=3)
         y_train = y_train.reshape(-1, 1)
 
         # Adversarial ground truths
         valid = np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
 
-        for epoch in range(epochs):
-
+        for epoch in range(1, epochs + 1):
             # ---------------------
             #  Train Discriminator
             # ---------------------
@@ -178,7 +173,7 @@ class ACGAN():
             # Generate a half batch of new images
             gen_imgs = self.generator.predict([noise, sampled_labels])
 
-            # Image labels. 0-9 
+            # Image labels. 0-9
             img_labels = y_train[idx]
 
             # Train the discriminator
@@ -194,38 +189,36 @@ class ACGAN():
             g_loss = self.combined.train_on_batch([noise, sampled_labels], [valid, sampled_labels])
 
             # Plot the progress
-            print ("%d [D loss: %f, acc.: %.2f%%, op_acc: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[3], 100*d_loss[4], g_loss[0]))
+            Hexnet_print(f'(epoch={epoch:5}/{epochs}) [D loss={d_loss[0]:11.8f}, acc={100*d_loss[3]:6.2f}%, op_acc={100*d_loss[4]:6.2f}%] [G loss={g_loss[0]:11.8f}]')
 
             # If at save interval => save generated image samples
-            if epoch % sample_interval == 0:
-                self.save_model()
-                self.sample_images(epoch)
+            if (not epoch or not epoch % sample_rate) and tests_dir is not None:
+                self.sample_images(epoch, tests_dir, run_title, images_to_sample_per_class)
 
-    def sample_images(self, epoch):
-        r, c = 10, 10
+
+    def sample_images(self, epoch, tests_dir, run_title, images_to_sample_per_class):
+        Hexnet_print('Sampling images')
+
+        r, c = images_to_sample_per_class, self.num_classes
         noise = np.random.normal(0, 1, (r * c, self.latent_dim))
         sampled_labels = np.array([num for _ in range(r) for num in range(c)])
         gen_imgs = self.generator.predict([noise, sampled_labels])
         # Rescale images 0 - 1
         gen_imgs = 0.5 * gen_imgs + 0.5
 
-        fig, axs = plt.subplots(r, c)
-        cnt = 0
-        for i in range(r):
-            for j in range(c):
-                axs[i,j].imshow(gen_imgs[cnt,:,:,0], cmap='gray')
-                axs[i,j].axis('off')
-                cnt += 1
-        fig.savefig("images/%d.png" % epoch)
-        plt.close()
+        tests_dir_samples = os.path.join(tests_dir, run_title)
+        os.makedirs(tests_dir_samples, exist_ok=True)
+
+        for image_counter, (image, label) in enumerate(zip(gen_imgs, sampled_labels)):
+            image_filename = f'epoch{epoch}_label{label}_image{image_counter}.png'
+            imsave(os.path.join(tests_dir_samples, image_filename), image)
+
 
     def save_model(self):
-
         def save(model, model_name):
             model_path = "saved_model/%s.json" % model_name
             weights_path = "saved_model/%s_weights.hdf5" % model_name
-            options = {"file_arch": model_path,
-                        "file_weight": weights_path}
+            options = {"file_arch": model_path, "file_weight": weights_path}
             json_string = model.to_json()
             open(options['file_arch'], 'w').write(json_string)
             model.save_weights(options['file_weight'])
@@ -236,7 +229,6 @@ class ACGAN():
 
 
 
-def model_GAN_ACGAN_standalone():
-    return ACGAN()
-
+def model_GAN_ACGAN_standalone(input_shape, classes, optimizer='adam'):
+    return ACGAN(input_shape, classes, optimizer)
 
