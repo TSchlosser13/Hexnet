38868dba095415c1508b953bdea70e09bb67cde1
Tobias Schlosser <tobias@tobias-schlosser.net>
2020-04-19 23:35:50 +0200

    [ML] updated models: updated Keras models

--- _ML/models/contrib/keras.py
+++ _ML/models/contrib/keras.py
@@ -28,58 +28,58 @@
 import tensorflow as tf
 
 
-def model_keras_DenseNet121(input_shape, classes):
-	return tf.keras.applications.DenseNet121(input_shape, classes)
+def model_keras_DenseNet121(input_shape, classes, weights=None):
+	return tf.keras.applications.DenseNet121(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_DenseNet169(input_shape, classes):
-	return tf.keras.applications.DenseNet169(input_shape, classes)
+def model_keras_DenseNet169(input_shape, classes, weights=None):
+	return tf.keras.applications.DenseNet169(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_DenseNet201(input_shape, classes):
-	return tf.keras.applications.DenseNet201(input_shape, classes)
+def model_keras_DenseNet201(input_shape, classes, weights=None):
+	return tf.keras.applications.DenseNet201(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_InceptionResNetV2(input_shape, classes):
-	return tf.keras.applications.InceptionResNetV2(input_shape, classes)
+def model_keras_InceptionResNetV2(input_shape, classes, weights=None):
+	return tf.keras.applications.InceptionResNetV2(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_InceptionV3(input_shape, classes):
-	return tf.keras.applications.InceptionV3(input_shape, classes)
+def model_keras_InceptionV3(input_shape, classes, weights=None):
+	return tf.keras.applications.InceptionV3(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_MobileNet(input_shape, classes):
-	return tf.keras.applications.MobileNet(input_shape, classes)
+def model_keras_MobileNet(input_shape, classes, weights=None):
+	return tf.keras.applications.MobileNet(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_MobileNetV2(input_shape, classes):
-	return tf.keras.applications.MobileNetV2(input_shape, classes)
+def model_keras_MobileNetV2(input_shape, classes, weights=None):
+	return tf.keras.applications.MobileNetV2(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_NASNetLarge(input_shape, classes):
-	return tf.keras.applications.NASNetLarge(input_shape, classes)
+def model_keras_NASNetLarge(input_shape, classes, weights=None):
+	return tf.keras.applications.NASNetLarge(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_NASNetMobile(input_shape, classes):
-	return tf.keras.applications.NASNetMobile(input_shape, classes)
+def model_keras_NASNetMobile(input_shape, classes, weights=None):
+	return tf.keras.applications.NASNetMobile(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_ResNet101(input_shape, classes):
-	return tf.keras.applications.ResNet101(input_shape, classes)
+def model_keras_ResNet101(input_shape, classes, weights=None):
+	return tf.keras.applications.ResNet101(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_ResNet101V2(input_shape, classes):
-	return tf.keras.applications.ResNet101V2(input_shape, classes)
+def model_keras_ResNet101V2(input_shape, classes, weights=None):
+	return tf.keras.applications.ResNet101V2(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_ResNet152(input_shape, classes):
-	return tf.keras.applications.ResNet152(input_shape, classes)
+def model_keras_ResNet152(input_shape, classes, weights=None):
+	return tf.keras.applications.ResNet152(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_ResNet152V2(input_shape, classes):
-	return tf.keras.applications.ResNet152V2(input_shape, classes)
+def model_keras_ResNet152V2(input_shape, classes, weights=None):
+	return tf.keras.applications.ResNet152V2(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_ResNet50(input_shape, classes):
-	return tf.keras.applications.ResNet50(input_shape, classes)
+def model_keras_ResNet50(input_shape, classes, weights=None):
+	return tf.keras.applications.ResNet50(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_ResNet50V2(input_shape, classes):
-	return tf.keras.applications.ResNet50V2(input_shape, classes)
+def model_keras_ResNet50V2(input_shape, classes, weights=None):
+	return tf.keras.applications.ResNet50V2(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_VGG16(input_shape, classes):
-	return tf.keras.applications.VGG16(input_shape, classes)
+def model_keras_VGG16(input_shape, classes, weights=None):
+	return tf.keras.applications.VGG16(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_VGG19(input_shape, classes):
-	return tf.keras.applications.VGG19(input_shape, classes)
+def model_keras_VGG19(input_shape, classes, weights=None):
+	return tf.keras.applications.VGG19(input_shape=input_shape, classes=classes, weights=weights)
 
-def model_keras_Xception(input_shape, classes):
-	return tf.keras.applications.Xception(input_shape, classes)
+def model_keras_Xception(input_shape, classes, weights=None):
+	return tf.keras.applications.Xception(input_shape=input_shape, classes=classes, weights=weights)
 
 
