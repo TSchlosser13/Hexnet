0aee24e63434676116e05e2ccf5812eb4645cb1f
Tobias Schlosser <tobias@tobias-schlosser.net>
2020-04-22 23:39:39 +0200

    [ML] updated visualization: added layer activation visualization

--- _ML/misc/visualization.py
+++ _ML/misc/visualization.py
@@ -25,9 +25,12 @@
  ****************************************************************************'''
 
 
+import cv2
 import os
 
 import matplotlib.pyplot as plt
+import numpy             as np
+import tensorflow        as tf
 
 from matplotlib.pyplot import imsave
 from tensorflow.keras  import Model
@@ -50,7 +53,7 @@ def visualize_filters(model, output_dir, verbosity_level=2):
 			Hexnet_print(f'(visualize_filters) layer={layer} (layer_counter={layer_counter}, layer.name={layer.name}): filters.shape={filters.shape}')
 
 		for channel in range(filters.shape[2]):
-			filter_filename = os.path.join(output_dir, f'layer{str(layer_counter).zfill(3)}_{layer.name}_filter{filter_to_visualize}_channel{channel}.png')
+			filter_filename = os.path.join(output_dir, f'layer{str(layer_counter).zfill(3)}_{layer.name}_channel{str(channel).zfill(3)}_filter{filter_to_visualize}.png')
 			filter          = filters[:, :, channel, filter_to_visualize]
 			imsave(filter_filename, filter, cmap='viridis')
 
@@ -68,6 +71,20 @@ def visualize_feature_maps(
 
 	os.makedirs(output_dir, exist_ok=True)
 
+	test_data_for_prediction   = []
+	test_labels_for_prediction = []
+
+	class_counter_dict = dict.fromkeys(test_classes, 0)
+
+	for image, label in zip(test_data, test_labels):
+		if class_counter_dict[label] < max_images_per_class:
+			test_data_for_prediction.append(image)
+			test_labels_for_prediction.append(label)
+			class_counter_dict[label] += 1
+
+	test_data_for_prediction   = np.asarray(test_data_for_prediction)
+	test_labels_for_prediction = np.asarray(test_labels_for_prediction)
+
 	layers_outputs = [layer.output for layer in model.layers]
 
 	if verbosity_level >= 3:
@@ -78,7 +95,7 @@ def visualize_feature_maps(
 	if verbosity_level >= 3:
 		Hexnet_print(f'(visualize_feature_maps) model_outputs={model_outputs}')
 
-	predictions = model_outputs.predict(test_data)
+	predictions = model_outputs.predict(test_data_for_prediction)
 
 	if verbosity_level >= 3:
 		Hexnet_print(f'(visualize_feature_maps) predictions={predictions}')
@@ -90,14 +107,81 @@ def visualize_feature_maps(
 		if verbosity_level >= 2:
 			Hexnet_print(f'(visualize_feature_maps) layer={layer} (layer_counter={layer_counter}, layer.name={layer.name}): feature_maps.shape={feature_maps.shape}')
 
-		class_counter_dict = dict.fromkeys(test_classes, 0)
+		for feature_map_counter, (feature_map, label) in enumerate(zip(feature_maps, test_labels_for_prediction)):
+			title = f'layer{str(layer_counter).zfill(3)}_{layer.name}_label{str(label).zfill(3)}_image{str(feature_map_counter).zfill(3)}_featuremap{feature_map_to_visualize}'
+			feature_map_filename = os.path.join(output_dir, f'{title}.png')
+			feature_map          = feature_map[:, :, feature_map_to_visualize]
+			imsave(feature_map_filename, feature_map, cmap='viridis')
 
-		for feature_map_counter, (feature_map, label) in enumerate(zip(feature_maps, test_labels)):
-			if class_counter_dict[label] < max_images_per_class:
-				feature_map_filename = os.path.join(output_dir, f'layer{str(layer_counter).zfill(3)}_{layer.name}_fm{feature_map_to_visualize}_label{label}_image{feature_map_counter}.png')
-				feature_map          = feature_map[:, :, feature_map_to_visualize]
-				imsave(feature_map_filename, feature_map, cmap='viridis')
-				class_counter_dict[label] += 1
+
+def visualize_activations(
+	model,
+	test_classes,
+	test_data,
+	test_labels,
+	output_dir,
+	max_images_per_class = 10,
+	verbosity_level      =  2):
+
+	heatmap_intensity_factor = 0.66
+
+	os.makedirs(output_dir, exist_ok=True)
+
+	test_data_for_prediction    = []
+	test_data_for_visualization = []
+	test_labels_for_prediction  = []
+
+	class_counter_dict = dict.fromkeys(test_classes, 0)
+
+	for image, label in zip(test_data, test_labels):
+		if class_counter_dict[label] < max_images_per_class:
+			test_data_for_prediction.append(image)
+			test_labels_for_prediction.append(label)
+			class_counter_dict[label] += 1
+
+	test_data_for_prediction    = np.asarray(test_data_for_prediction, dtype=np.float32)
+	test_data_for_visualization = np.clip(255 * (test_data_for_prediction + 0.5), 0, 255)
+	test_labels_for_prediction  = np.asarray(test_labels_for_prediction)
+
+	for layer_counter, layer in enumerate(model.layers):
+		if not 'conv' in layer.name:
+			continue
+
+		if verbosity_level >= 2:
+			Hexnet_print(f'(visualize_activations) layer={layer} (layer_counter={layer_counter}, layer.name={layer.name}): layer.output.shape={layer.output.shape}')
+
+		model_outputs = Model(inputs = model.inputs, outputs = (model.output, layer.output))
+
+		with tf.GradientTape() as gradient_tape:
+			predictions, layer_output = model_outputs(test_data_for_prediction)
+			predictions = tf.reduce_max(predictions, axis=1)
+			gradients   = gradient_tape.gradient(predictions, layer_output)
+			gradients   = tf.reduce_mean(gradients, axis = (1, 2))
+
+		activations = tf.einsum('ijkl,il->ijkl', layer_output, gradients)
+		activations = tf.reduce_mean(activations, axis=3)
+		activations = np.maximum(activations, 0)
+
+		for image_counter, (image, label, activation) in enumerate(zip(test_data_for_visualization, test_labels_for_prediction, activations)):
+			activation_max = np.max(activation)
+
+			if activation_max:
+				activation /= activation_max
+
+			activation = (255 * activation).astype(np.uint8)
+			activation = cv2.resize(activation, (image.shape[1], image.shape[0]))
+			activation = cv2.equalizeHist(activation)
+			heatmap    = cv2.applyColorMap(activation, cv2.COLORMAP_VIRIDIS)
+
+			image_heatmapped = image + heatmap_intensity_factor * heatmap
+			image_heatmapped = np.clip(image_heatmapped, 0, 255).astype(np.uint8)
+
+			title = f'layer{str(layer_counter).zfill(3)}_{layer.name}_label{str(label).zfill(3)}_image{str(image_counter).zfill(3)}'
+			image_heatmapped_filename = os.path.join(output_dir, f'{title}_image_heatmapped.png')
+			heatmap_filename          = os.path.join(output_dir, f'{title}_activations_heatmap.png')
+
+			imsave(image_heatmapped_filename, image_heatmapped)
+			imsave(heatmap_filename,          heatmap)
 
 
 def visualize_model(
@@ -114,6 +198,15 @@ def visualize_model(
 	visualize_filters(model, output_dir, verbosity_level)
 
 	visualize_feature_maps(
+		model,
+		test_classes,
+		test_data,
+		test_labels,
+		output_dir,
+		max_images_per_class,
+		verbosity_level)
+
+	visualize_activations(
 		model,
 		test_classes,
 		test_data,
