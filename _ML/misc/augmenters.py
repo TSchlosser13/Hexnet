f40f8ba0b75af5fe87303568b021ab6c2bba97e1
Tobias Schlosser <tobias@tobias-schlosser.net>
2020-09-28 23:52:26 +0200

    [ML] updated augmenters: added hexagonal augmenter functionality

--- _ML/misc/augmenters.py
+++ _ML/misc/augmenters.py
@@ -25,8 +25,14 @@
  ****************************************************************************'''
 
 
+import random
+
 import imgaug            as ia
 import imgaug.augmenters as iaa
+import numpy             as np
+
+from core.Hexarray import rotate_Hexarray, scale_Hexarray, translate_Hexarray
+from misc.misc     import test_image_batch
 
 
 def augmenter_simple(augmentation_level=1):
@@ -67,18 +73,18 @@ def augmenter_complex_custom():
 			iaa.Flipud(0.2), # vertically flip 20% of all images
 			# crop images by -5% to 10% of their height/width
 			sometimes(iaa.CropAndPad(
-				percent=(-0.05, 0.1),
-				pad_mode=ia.ALL,
-				pad_cval=(0, 255)
+				percent  = (-0.05, 0.1),
+				pad_mode = ia.ALL,
+				pad_cval = (0, 255)
 			)),
 			sometimes(iaa.Affine(
-				scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, individually per axis
-				translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
-				rotate=(-45, 45), # rotate by -45 to +45 degrees
-				shear=(-16, 16), # shear by -16 to +16 degrees
-				order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
-				cval=(0, 255), # if mode is constant, use a cval between 0 and 255
-				mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+				scale             = {"x": (0.8, 1.2), "y": (0.8, 1.2)},   # scale images to 80-120% of their size, individually per axis
+				translate_percent = {"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent (per axis)
+				rotate            = (-45, 45),                            # rotate by -45 to +45 degrees
+				shear             = (-16, 16),                            # shear by -16 to +16 degrees
+				order             = [0, 1],                               # use nearest neighbour or bilinear interpolation (fast)
+				cval              = (0, 255),                             # if mode is constant, use a cval between 0 and 255
+				mode              = ia.ALL                                # use any of scikit-image's warping modes (see 2nd image from the top for examples)
 			)),
 			# execute 0 to 5 of the following (less important) augmenters per image
 			# don't execute all of them, as that would often be way too strong
@@ -87,11 +93,11 @@ def augmenter_complex_custom():
 					sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
 					iaa.OneOf([
 						iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
-						iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
-						iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+						iaa.AverageBlur(k=(2, 7)),  # blur image using local means with kernel sizes between 2 and 7
+						iaa.MedianBlur(k=(3, 11)),  # blur image using local medians with kernel sizes between 2 and 7
 					]),
 					iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
-					iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+					iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),      # emboss images
 					# search either for all edges or for directed edges,
 					# blend the result with the original image using a blobby mask
 					iaa.SimplexNoiseAlpha(iaa.OneOf([
@@ -103,23 +109,23 @@ def augmenter_complex_custom():
 						iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
 						iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
 					]),
-					iaa.Invert(0.05, per_channel=True), # invert color channels
-					iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
+					iaa.Invert(0.05, per_channel=True),   # invert color channels
+					iaa.Add((-10, 10), per_channel=0.5),  # change brightness of images (by -10 to 10 of original value)
 					iaa.AddToHueAndSaturation((-20, 20)), # change hue and saturation
 					# either change the brightness of the whole image (sometimes
 					# per channel) or change the brightness of subareas
 					iaa.OneOf([
 						iaa.Multiply((0.5, 1.5), per_channel=0.5),
 						iaa.FrequencyNoiseAlpha(
-							exponent=(-4, 0),
-							first=iaa.Multiply((0.5, 1.5), per_channel=True),
-							second=iaa.LinearContrast((0.5, 2.0))
+							exponent = (-4, 0),
+							first    = iaa.Multiply((0.5, 1.5), per_channel=True),
+							second   = iaa.LinearContrast((0.5, 2.0))
 						)
 					]),
 					iaa.LinearContrast((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
 					iaa.Grayscale(alpha=(0.0, 1.0)),
 					sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
-					sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))), # sometimes move parts of the image around
+					sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))),                 # sometimes move parts of the image around
 					sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
 				],
 				random_order=True
@@ -130,4 +136,68 @@ def augmenter_complex_custom():
 
 
 	return augmenter
+
+
+
+
+def randomized_rotate_Hexarray(Hexarray_s):
+	Hexarray_s = test_image_batch(Hexarray_s)
+
+	angle_factor         = 60
+	angle_selector_range = (1, 5)
+
+	angle_selector = random.randint(angle_selector_range)
+	angle          = angle_selector * angle_factor
+
+	Hexarray_s_rotated = rotate_Hexarray(Hexarray_s, angle)
+
+	return Hexarray_s_rotated
+
+
+def randomized_scale_Hexarray(Hexarray_s):
+	Hexarray_s = test_image_batch(Hexarray_s)
+
+	res_factor         = Hexarray_s.shape[1:3]
+	res_selector_range = (0.5, 2)
+
+	res_selector = random.uniform(res_selector_range)
+	res          = np.round(res_selector * res_factor)
+
+	Hexarray_s_scaled = scale_Hexarray(Hexarray_s, res, method = 0, fill_value = 0)
+
+	return Hexarray_s_scaled
+
+
+def randomized_translate_Hexarray(Hexarray_s):
+	Hexarray_s = test_image_batch(Hexarray_s)
+
+	translation_factor         = Hexarray_s.shape[1:3]
+	translation_selector_range = (-0.5, 0.5)
+
+	translation_selector = np.random.uniform(translation_selector_range, size=2)
+	translation          = np.round(translation_selector * translation_factor)
+
+	Hexarray_s_translated = translate_Hexarray(Hexarray_s, translation, axis = (1, 2), fill_value = 0, cyclic_translation = False)
+
+	return Hexarray_s_translated
+
+
+class augmenter_simple_hex_custom():
+	def __init__(self):
+		self.augmentation_functions = (randomized_rotate_Hexarray, randomized_scale_Hexarray, randomized_translate_Hexarray)
+
+	def __call__(self, images):
+		images_augmented = []
+
+		for image in images:
+			augmentation_selectors = np.random.random(size=3) < 0.5
+
+			for augmentation_function, augmentation_selector in zip(self.augmentation_functions, augmentation_selectors):
+				if augmentation_selector:
+					image = augmentation_function(image)
+
+			images_augmented.append(image)
+
+		return images_augmented
+
 
